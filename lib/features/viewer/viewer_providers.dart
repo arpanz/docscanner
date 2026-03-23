dff035d1309ea5a8bb143e4f63864a5a5c7df907
Arpan singh <69468492+arpanz@users.noreply.github.com>
2026-03-23 13:19:17 +0530

    feat: add features/viewer/viewer_providers.dart

---
+++ lib/features/viewer/viewer_providers.dart
@@ -0,0 +1,17 @@
+// lib/features/viewer/viewer_providers.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../database/app_database.dart';
+import '../../database/tables.dart';
+
+final documentProvider =
+    StreamProvider.family<Document?, int>((ref, docId) {
+  return ref
+      .watch(documentsDaoProvider)
+      .getDocument(docId)
+      .asStream();
+});
+
+final documentPagesProvider =
+    StreamProvider.family<List<Page>, int>((ref, docId) {
+  return ref.watch(pagesDaoProvider).watchPagesForDocument(docId);
+});
