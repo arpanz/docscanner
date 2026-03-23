ed548797968a8dad557cf1bc968844861c19e295
Arpan singh <69468492+arpanz@users.noreply.github.com>
2026-03-23 13:13:38 +0530

    feat: add database/tables.dart (Drift table definitions)

---
+++ lib/database/tables.dart
@@ -0,0 +1,26 @@
+// lib/database/tables.dart
+import 'package:drift/drift.dart';
+
+// ---------------------------------------------------------------------------
+// Documents table
+// ---------------------------------------------------------------------------
+class Documents extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text().withLength(min: 1, max: 200)();
+  DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
+  DateTimeColumn get updatedAt => dateTime().withDefault(currentDateAndTime)();
+  IntColumn get pageCount => integer().withDefault(const Constant(0))();
+  TextColumn get coverPagePath => text().nullable()();
+  BoolColumn get isFavourite => boolean().withDefault(const Constant(false))();
+}
+
+// ---------------------------------------------------------------------------
+// Pages table
+// ---------------------------------------------------------------------------
+class Pages extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get documentId => integer().references(Documents, #id)();
+  TextColumn get imagePath => text()();
+  IntColumn get pageIndex => integer()();
+  DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
+}
