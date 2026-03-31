a5b2f283664809fa50a1541be7dd5b05e09c1408
arpanz <arpansinghbsb@gmail.com>
2026-03-31 15:13:11 +0530

    *

--- lib/features/camera/camera_page_native.dart
+++ lib/features/camera/camera_page_native.dart
@@ -1,6 +1,7 @@
 // lib/features/camera/camera_page_native.dart
 import 'dart:async';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
